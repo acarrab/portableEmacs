93ec900ac7622d3ec48c132ccdeda2038ea2a3f9
Angelo Carrabba <acarrabba6@gmail.com>
2017-02-22 01:48:57 -0500

    Quick push

---
+++ elpa/babel-20161122.2340/babel-pkg.el
@@ -0,0 +1,1 @@
+(define-package "babel" "20161122.2340" "interface to web translation services such as Babelfish" 'nil :url "http://github.com/juergenhoetzel/babel" :keywords '("translation" "web"))

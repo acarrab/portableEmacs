04473bef9e35727a06f243dac55f91deb7fd0547
Angelo Carrabba <acarrabba6@gmail.com>
2017-03-01 20:39:08 -0500

    Quick push

--- gen.py
+++ gen.py
@@ -3,19 +3,19 @@ import pexpect, re
 from sys import argv
 
 emacs_d = '~/.emacs.d/'
-excludeFiles = set(['README.org', 'gen.py', 'Makefile'])
-files = pexpect.run('ls').split()
+files = ['loader.org', 'init.el']
 cmd = None
+
 if len(argv) == 1 or argv[1] == "emacs.d":
     cmd = lambda f: 'cp ' + f + ' ' + emacs_d + f
 elif argv[1] == "github":
     cmd = lambda f: 'cp ' + emacs_d + f + ' ' + f
-
+else:
+    print(argv[1] + " is not defined.")
+    return
 
 
 for f in files:
-    if f in excludeFiles: continue
-    if re.match(r'.*~',f): continue
     print(cmd(f))
     res = pexpect.run('/bin/bash -c "%s"'%cmd(f))
     if len(res): print(res)

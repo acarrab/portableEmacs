cb2bcf339a42c1e1b331a2f276da4c0e1b5a3a41
Angelo Carrabba <acarrabba6@gmail.com>
2017-03-01 21:58:20 -0500

    Changed the font size

--- init.el
+++ init.el
@@ -33,4 +33,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 181 :width normal :powerline-scale 1.1)))))
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 128 :width normal :powerline-scale 1.1)))))

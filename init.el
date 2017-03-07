3ae12b86fd0373a9fe362400e5612c9cb31718ec
Angelo Carrabba <acarrabba6@gmail.com>
2017-03-07 14:09:53 -0500

    Quick push

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
  '(custom-safe-themes
    (quote
     ("b9b1a8d2ec1d5c17700e1a09256f33c2520b26f49980ed9e217e444c381279a9" default)))
+ '(debug-on-error t)
  '(display-battery-mode t)
  '(display-time-mode t)
  '(inhibit-default-init t)
@@ -25,6 +26,9 @@
  '(inhibit-startup-screen t)
  '(initial-major-mode (quote org-mode))
  '(initial-scratch-message nil)
+ '(package-selected-packages
+   (quote
+    (ox-gfm ox-twbs forecast which-key web-mode virtualenv use-package try suscolors-theme smex shader-mode rainbow-delimiters powerline org-bullets org-babel-eval-in-repl ob-ipython mode-icons magit jedi ivy-hydra importmagic highlight-parentheses git-gutter-fringe ess ein-mumamo edit-server dired-k counsel babel anaconda-mode)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 
@@ -33,4 +37,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 128 :width normal :powerline-scale 1.1)))))
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 163 :width normal :powerline-scale 1.1)))))
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)

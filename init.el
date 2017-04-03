e7019fa45939b7fd704d2d90446dff38b2274080
Angelo Carrabba <acarrabba6@gmail.com>
2017-04-03 15:04:34 -0400

    Quick push

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 163 :width normal :powerline-scale 1.1)))))
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 140 :width normal :powerline-scale 1.1)))))
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
